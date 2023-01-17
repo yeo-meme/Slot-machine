a049f1c4f68e7c44e95c0631df817568032e2c2d
yeomeme <yeomeme@gmail.com>
2023-01-17 15:38:24 +0900

    first commit

---
+++ Slot-machine/Slot_machineApp.swift
@@ -0,0 +1,17 @@
+//
+//  Slot_machineApp.swift
+//  Slot-machine
+//
+//  Created by yeomim Kim on 2023/01/09.
+//
+
+import SwiftUI
+
+@main
+struct Slot_machineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
